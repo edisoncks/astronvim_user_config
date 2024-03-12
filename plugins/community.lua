a2aa9258e5d01ebe1bf94023344f7cff151c5b89
edisoncks <edison.chong.cks@gmail.com>
2024-03-12 22:27:43 +0800

    codeium custom plugin config

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,7 +7,6 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 
-  { import = "astrocommunity.completion.codeium-vim" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.html-css" },
